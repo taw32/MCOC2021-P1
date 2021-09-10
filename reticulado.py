aee0f0baa40dcd8cee937d8963d8f0921b371c84
pablo14simon <88359228+pablo14simon@users.noreply.github.com>
2021-09-10 18:20:14 -0300

    Update reticulado.py

--- reticulado.py
+++ reticulado.py
@@ -3,36 +3,32 @@ from scipy.linalg import solve
 
 class Reticulado(object):
     """Define un reticulado"""
-    __NNodosInit__ = 100
+    _NNodosInit_ = 100
 
     #constructor
-    def __init__(self):
-        super(Reticulado, self).__init__()
+    def _init_(self):
+        super(Reticulado, self)._init_()
         
         print("Constructor de Reticulado")
         
-        self.xyz = np.zeros((Reticulado.__NNodosInit__,3), dtype=np.double)
+        self.xyz = np.zeros((Reticulado._NNodosInit_,3), dtype=np.double)
         self.Nnodos = 0
         self.barras = []
         self.cargas = {}
         self.restricciones = {}
-        """Implementar"""	
-        
-
-
+    
     def agregar_nodo(self, x, y, z=0):
-        
-        """Implementar"""	
+	
 
         print(f"Quiero agregar un nodo en ({x} {y} {z})")
         numero_de_nodo_actual = self.Nnodos
-
-        self.xyz[numero_de_nodo_actual,:] = [x, y, z]
-
-        self.Nnodos += 1
-        
-        return 0
-
+        if self.Nnodos+1 > Reticulado._NNodosInit_:
+    		self.xyz.resize((self.Nnodos+1,3))
+    		self.xyz[self.Nnodos,:] = [x, y, z]
+    		self.Nnodos += 1
+    		if z != 0.:
+    			self.Ndimensiones = 3
+    
     def agregar_barra(self, barra):
         
         self.barras.append(barra)        
@@ -41,13 +37,18 @@ class Reticulado(object):
 
     def obtener_coordenada_nodal(self, n):
         
-        """Implementar"""	
+        if n >= self.Nnodos:
+			return 
+		return self.xyz[n, :]	
         
         return 0
 
     def calcular_peso_total(self):
         
-        """Implementar"""	
+        peso = 0.
+		for b in self.barras:
+			peso += b.calcular_peso(self)
+		return peso	
         
         return 0
 
@@ -58,7 +59,6 @@ class Reticulado(object):
     def obtener_barras(self):
         
         return self.barras
-
 
 
     def agregar_restriccion(self, nodo, gdl, valor=0.0):
@@ -124,16 +124,18 @@ class Reticulado(object):
 
 
 
-
-
-
-
     def __str__(self):
-
-        s = "Soy un reticulado :)"
-
-        s += "\n"
         
-        s += str(self.xyz[0 : self.Nnodos,:])
-
+        s = "nodos: \n"
+        for i in range(len(self.xyz)):
+            s+= f"\t {i}: ({self.obtener_coordenada_nodal(i)}) \n"
+        
+        s+="\n"
+        s += "barras: \n"
+        for i,j in enumerate(self.barras,start=0):
+            s+= f"\t {i}: [{j.ni} {j.nj}] \n"
+        
+        s+="\n"
+        
         return s
+
